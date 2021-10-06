7f06475edf326c31d70a82729e270f71878f03d8
andrewfullard <andrewfullard@users.noreply.github.com>
2021-10-06 14:02:19 +0000

    deploy: c1b507764d9f894bc17412fdb9c26e197adda51a

---
+++ _static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '2021.10.6.0.dev4+gc1b50776',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
